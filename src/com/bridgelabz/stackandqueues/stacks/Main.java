b38b2b624596e3cc71cb08db3940bcf54cfc1a89
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-09-01 13:11:37 +0530

    Modified: peak and pop the head element in the list until list is empty

--- src/com/bridgelabz/stackandqueues/stacks/Main.java
+++ src/com/bridgelabz/stackandqueues/stacks/Main.java
@@ -8,5 +8,12 @@ public class Main {
         stack.push(30);
         stack.push(56);
         stack.print();
+        System.out.println();
+        while(!stack.isEmpty()){
+            System.out.println(stack.peek());
+            stack.pop();
+            stack.print();
+            System.out.println();
+        }
     }
 }
--- src/com/bridgelabz/stackandqueues/stacks/Stack.java
+++ src/com/bridgelabz/stackandqueues/stacks/Stack.java
@@ -2,7 +2,7 @@ package com.bridgelabz.stackandqueues.stacks;
 
 import com.bridgelabz.stackandqueues.linkedlist.*;
 
-  public class Stack<T> {
+public class Stack<T> {
 
     LinkedList<T> linkedList;
 
@@ -22,5 +22,16 @@ import com.bridgelabz.stackandqueues.linkedlist.*;
 
     public T pop() {
         return linkedList.pop();
+    }
+
+    public T peek(){
+        return linkedList.peek();
+    }
+
+    public boolean isEmpty(){
+        if(linkedList.size() == 0)
+            return true;
+        else
+            return false;
     }
 }
